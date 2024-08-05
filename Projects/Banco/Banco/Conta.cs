5b157a9e7f4e4944e4175797f2cec5e6e41ff5c9
Vitor Fernandes de Souza <vifersouza@gmail.com>
2024-08-05 15:49:41 -0300

    Banco
    Console App simples de um exemplos de banco

---
+++ Projects/Banco/Banco/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Banco
+{
+    public class Conta
+    {
+        public int Taxa { get; private set; } = 5;
+        public int IdConta;
+        public string Titular;
+        public double Saldo;
+
+        public Conta(int idConta, string titular, double saldo)
+        {
+            IdConta = idConta;
+            Titular = titular;
+            Saldo = 0;
+        }
+
+        public void Deposito(double saldoIncial)
+        {
+            Saldo = saldoIncial;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo = Saldo - (saque + Taxa);
+        }
+        public override string ToString()
+        {
+            return $"Conta: {IdConta}, Titular: {Titular}, Saldo: {Saldo:C}";
+        }
+
+    }
+}
---
+++ Projects/Banco/Banco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Banco // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é o número da conta? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual o nome do Titular da conta? ");
+            string nome = Console.ReadLine();
+
+            Conta conta = new Conta(n, nome, 0);
+
+            Console.WriteLine("Deseja fazer um deposito inicial? s/n ");
+            char resp = char.Parse(Console.ReadLine().ToLower());
+
+            if (resp == 's')
+            {
+                Console.WriteLine("Qual o valor do deposito inicial? ");
+                double dp = double.Parse(Console.ReadLine());
+                conta.Deposito(dp);
+            }
+
+            Console.WriteLine("Dejesa sacar algum valor? s/n ");
+            char resposta = char.Parse(Console.ReadLine().ToLower());
+
+            if (resposta == 's')
+            {
+                Console.WriteLine($"Lembre - se que existe uma taxa fixa de {conta.Taxa} reais para saques");
+                Console.WriteLine("Digite o valor que deseja sacar? ");
+                double saque = double.Parse(Console.ReadLine());
+                if (saque > conta.Saldo - conta.Taxa)
+                {
+                    Console.WriteLine("Saldo insuficiente!");
+                }
+                else
+                {
+                    conta.Saque(saque);
+                    Console.WriteLine("Dados atualizados");
+                }
+            }
+            Console.WriteLine(conta);
+
+
+
+        }
+    }
+}
