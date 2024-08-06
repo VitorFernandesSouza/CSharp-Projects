16c23c5b97d02dc91c2b1a5c0580a44d4f8cb70c
Vitor Fernandes de Souza <vifersouza@gmail.com>
2024-08-06 12:24:20 -0300

    Matriz
    Matriz

--- Projects/Lista/Source/obj/Release/net8.0/Listas.AssemblyInfo.cs
+++ Projects/Lista/Source/obj/Release/net8.0/Listas.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Listas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b157a9e7f4e4944e4175797f2cec5e6e41ff5c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Listas")]
---
+++ Projects/Matriz/Matriz/Matriz/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Matriz // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite a qtd de linhas da matriz: ");
+            int linhas = int.Parse(Console.ReadLine());
+            Console.Write("Digite a qtd de colunas da matriz: ");
+            int colunas = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[linhas, colunas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < colunas; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.Write("Digite um número para procurar na matriz: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Posição " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Esquerda: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Acima: " + mat[i - 1, j]);
+                        }
+                        if (j < colunas - 1)
+                        {
+                            Console.WriteLine("Direita: " + mat[i, j + 1]);
+                        }
+                        if (i < linhas - 1)
+                        {
+                            Console.WriteLine("Abaixo: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
