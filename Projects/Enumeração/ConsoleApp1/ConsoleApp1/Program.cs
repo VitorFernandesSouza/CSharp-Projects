85c16cbeda83652687246a9ab6d60d4d1d0ff54e
Vitor Fernandes de Souza <vifersouza@gmail.com>
2024-08-06 16:10:46 -0300

    Enum
    Enum

---
+++ Projects/Enumeração/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1.Entities;
+using ConsoleApp1.Entities.Enums;
+using System;
+
+namespace ConsoleApp1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order pedido = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment,
+            };
+
+            Console.WriteLine(pedido);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); 
+            Console.WriteLine(os);
+
+        }
+    }
+}
