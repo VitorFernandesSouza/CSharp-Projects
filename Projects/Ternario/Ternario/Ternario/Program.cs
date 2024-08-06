bfc2d591a6c9977133baf4e2324a8dccfe49a067
Vitor Fernandes de Souza <vifersouza@gmail.com>
2024-08-06 12:47:26 -0300

    If Ternário
    If Ternário

---
+++ Projects/Ternario/Ternario/Ternario/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Case // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto =   (preco > 20)  ?     desconto = preco * 0.1    :     desconto = preco * 0.05;
+//                              (condição)    ?     valor se for verdadeiro   :     valor se for falso
+            Console.WriteLine(desconto + preco);
+
+
+        }
+    }
+}
