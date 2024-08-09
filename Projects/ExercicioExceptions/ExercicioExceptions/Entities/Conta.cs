294392adfdfb418d55783de59ee1a5137647c926
Vitor Fernandes de Souza <vitor.souza.1388706@sga.pucminas.br>
2024-08-09 11:48:58 -0300

    ExercicioFixaçãoExceptions
    ExercicioFixaçãoExceptions

---
+++ Projects/ExercicioExceptions/ExercicioExceptions/Entities/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioExceptions.Entities.Exceptions;
+
+namespace ExercicioExceptions.Entities
+{
+     class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteSaque { get; set; }
+
+        public Conta() { }
+        public Conta(int numero, string titular, double saldo, double limiteSaque)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+            LimiteSaque = limiteSaque;
+        }
+
+        public void Deposito(double quantia)
+        {
+             Saldo += quantia;
+        }
+        public void Saque(double quantia)
+        {
+            if (quantia > LimiteSaque)
+            {
+                throw new DomainException("Erro no saque: A quantia excede o limite de saque, paizão");
+            }
+            if (quantia > Saldo)
+            {
+                throw new DomainException("Erro no saque: Saldo insuficiente, aí não da né paizão");
+            }
+            Saldo -= quantia + 5.0;
+        }
+
+
+    }
+}
---
+++ Projects/ExercicioExceptions/ExercicioExceptions/Program.cs
@@ -0,0 +1,56 @@
+﻿using ExercicioExceptions.Entities;
+using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Globalization;
+using ExercicioExceptions.Entities.Exceptions;
+
+namespace ExercicioExceptions // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bem Vindo ao Banco!");
+                Console.WriteLine("Digite os dados da conta:");
+                Console.Write("Numero: ");
+                int numero = int.Parse(Console.ReadLine());
+                Console.Write("Titular: ");
+                string titular = Console.ReadLine();
+                Console.Write("Saldo inicial: ");
+                double saldo = double.Parse(Console.ReadLine());
+                Console.Write("Limite de saque: ");
+                double limiteEmprestimo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Conta conta = new Conta(numero, titular, saldo, limiteEmprestimo);
+
+                Console.WriteLine();
+                Console.Write("Digite um valor para depósito:");
+                double quantiaDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Deposito(quantiaDeposito);
+
+                Console.Write("Digite o valor do saque: ");
+                double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Saque(quantia);
+
+
+                Console.WriteLine("Novo saldo: " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro de formato: " + e.Message);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Erro na conta: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro inesperado: " + e.Message);
+            }
+        }
+    }
+}
