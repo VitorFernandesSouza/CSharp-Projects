675e3884c811727b47baabd1c1dbc6336ec5d708
Vitor Fernandes de Souza <vitor.souza.1388706@sga.pucminas.br>
2024-08-16 16:00:00 -0300

    Dellagates
    Dellagates

---
+++ Projects/ConsoleApp/Dellagates/Entities/Produtos.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Dellagates.Entities
+{
+    public class Produtos
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produtos(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Preco.ToString("F2");
+        }
+    }
+}
---
+++ Projects/ConsoleApp/Dellagates/Program.cs
@@ -0,0 +1,30 @@
+﻿using Dellagates.Entities;
+namespace Dellagates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Predicate 
+
+            List<Produtos> list = new List<Produtos>();
+
+            list.Add(new Produtos("TV", 900.00));
+            list.Add(new Produtos("Mouse", 50.00));
+            list.Add(new Produtos("Tablet", 350.50));
+            list.Add(new Produtos("HD Case", 80.90));
+
+            list.RemoveAll(ProdutosDell);
+
+            foreach (Produtos p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProdutosDell(Produtos p)
+        {
+            return p.Preco >= 100.0;
+        }
+    }
+}
