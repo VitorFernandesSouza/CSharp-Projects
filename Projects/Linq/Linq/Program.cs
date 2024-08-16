f68238109df46181d8d30b60735ca879f14ef082
Vitor Fernandes de Souza <vitor.souza.1388706@sga.pucminas.br>
2024-08-16 10:53:29 -0300

    Introdução ao Linq
    Introdução ao Linq

---
+++ Projects/Linq/Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vetor de inteiros
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Consulta LINQ para filtrar os números pares e multiplicar por 10
+            var resultado = numeros.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Foreach para exibir o resultado
+            foreach (var item in resultado)
+            {
+                Console.WriteLine(item);                    
+            }
+
+        }
+    }
+}
