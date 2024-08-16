920330fe717d080e68c6ef841f68596e7e4f6ce5
Vitor Fernandes de Souza <vitor.souza.1388706@sga.pucminas.br>
2024-08-16 16:19:26 -0300

    Action
    Action

---
+++ Projects/ActionProject/Action/Entities/Produtos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action.Entities
+{
+    class Produtos
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produtos()
+        {
+        }
+
+        public Produtos(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        override public string ToString()
+        {
+            return Nome + ", " + Preco.ToString("F2");
+        }
+    }
+}
---
+++ Projects/ActionProject/Action/Program.cs
@@ -0,0 +1,26 @@
+﻿using Action.Entities;
+namespace Action
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produtos> list = new List<Produtos>();
+
+            list.Add(new Produtos("TV", 900.00));
+            list.Add(new Produtos("Mouse", 50.00));
+            list.Add(new Produtos("Tablet", 350.50));
+            list.Add(new Produtos("HD Case", 80.90));
+
+            // Quando uma função é void e queremos criar um labda com ela, precisamos coloca-lá entre parenteses
+            Action<Produtos> act = p => { p.Preco += p.Preco * 0.1; };
+
+            list.ForEach(p => { p.Preco += p.Preco * 0.1; }); // pode ser feito assim tbm
+
+            foreach (Produtos p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
