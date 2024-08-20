991cf4f41e1a5f791e553810706f667020b81e95
Vitor Fernandes de Souza <vitor.souza.1388706@sga.pucminas.br>
2024-08-20 11:40:44 -0300

    ExFixacao
    ExFixacao

---
+++ Projects/ExFixacao/ExFixacao/Entities/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFixacao.Entities
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public Pessoa(string nome, string email, double salario)
+        {
+            Nome = nome;
+            Email = email;
+            Salario = salario;
+        }
+    }
+}
---
+++ Projects/ExFixacao/ExFixacao/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExFixacao.Entities;
+
+namespace ExFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cole aqui o caminho do arquivo: ");
+            string arquivo = Console.ReadLine();
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            using (StreamReader sr = File.OpenText(arquivo))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] campos = sr.ReadLine().Split(',');
+                    string nome = campos[0];
+                    string email = campos[1];
+                    double salario = double.Parse(campos[2], System.Globalization.CultureInfo.InvariantCulture);
+                    pessoas.Add(new Pessoa(nome, email, salario));
+                }
+            }
+            Console.Write("Entre com a faixa salarial que deseja filtrar: ");
+            double faixa = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            Console.WriteLine("Email das pessoa que tem a faixa salarial maior que " + faixa);
+
+            var emailOrdem = pessoas.Where(p => p.Salario > faixa).OrderByDescending(p => p.Email).Select(p => p.Email);
+
+            foreach (var m in emailOrdem)
+            {
+                Console.WriteLine(m);
+            }
+
+        }
+    }
+}
