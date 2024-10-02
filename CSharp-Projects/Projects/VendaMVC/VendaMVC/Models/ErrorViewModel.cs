a8e88c80788d81c3330178982002cab0759a76c4
Vitor Fernandes de Souza <vitor.souza.1388706@sga.pucminas.br>
2024-10-02 12:02:10 -0300

    update

---
+++ CSharp-Projects/Projects/VendaMVC/VendaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
